eb3c2df465262aafac8d1daeb636f78d60b395bc
Anchel17 <anchel.varela@gmail.com>
2023-01-04 13:56:24 -0300

    js inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+const modalEquipe = document.querySelector(".modal-equipe")
+const tbody = document.querySelector("tbody");
+const teamName = document.querySelector("#modal-team-name");
+const teamHome = document.querySelector("#modal-team-home");
+const teamBudget = document.querySelector("#modal-team-budget");
+const addTeamBtn = document.querySelector("cria-equipe");
